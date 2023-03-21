371d6914754bba256f36c75cb60ad679c65dbbfc
= RizqiRHuda <= rizqirhuda45@gmail.com>
2023-03-21 16:00:24 +0700

    upload praktikum 3

---
+++ database/seeders/NewUserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\facades\DB;
+use Illuminate\Support\facades\Hash;
+
+
+class NewUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'username' => 'admin',
+            'name' => 'Administrator Baru',
+            'email'=> 'admin.baru@admin.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
